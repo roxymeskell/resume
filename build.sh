550569910ca7358b375c9a0c6ea860c268f766f2
roxymeskell <roxy.meskell@gmail.com>
2018-09-11 17:56:19 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e # Quit on error
+
+SHA=`git rev-parse --short --verify HEAD` # Save object name of the current commit
+
+# Compile
+mkdir _build
+pdflatex -interaction=nonstopmode -output-directory _build resume.tex
+
+# Create everything for gh-pages
+mkdir pages
+cp _build/resume.pdf pages/resume.pdf
+
+# Create README
+echo "# Automatic build" > pages/README.md
+echo "[View PDF](http:/roxymeskell.github.io/resume/resume.pdf)" >> pages/README.md
+echo "Built from \`$SHA\`. See https://github.com/roxymeskell/resume/ for details." >> pages/README.md
